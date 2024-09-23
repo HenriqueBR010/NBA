aa7e5088e8877e383d566ff1a92a6d7c38500362
HenriqueBR010 <168148276+HenriqueBR010@users.noreply.github.com>
2024-09-23 20:52:33 -0300

    NBA Project

---
+++ cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
@@ -0,0 +1,22 @@
+package usa.com.nba.atletas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class operacoesController {
+	
+	@FXML
+	private TextField txfNome;
+	@FXML
+	private TextField txfSobrenome;
+	@FXML
+	private TextField txfIdade;
+	@FXML
+	private TextField txfPosicao;
+	@FXML
+	private TextField fxmlEquipe;
+	@FXML
+	private Button btnCadastrar;
+	
+}
